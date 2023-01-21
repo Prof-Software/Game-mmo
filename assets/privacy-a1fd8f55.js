e7a2b77fbd588980b1bc11e24be44d166476501f
kartikeya <kartikeyasharma2006@gmail.com>
2023-01-21 19:22:38 +0530

    first commit

---
+++ assets/privacy-a1fd8f55.js
@@ -0,0 +1,2 @@
+function t(){import("data:text/javascript,")}export{t as __vite_legacy_guard};
+//# sourceMappingURL=privacy-a1fd8f55.js.map
---
+++ assets/privacy-legacy-518b8698.js
@@ -0,0 +1,2 @@
+System.register([],(function(e,t){"use strict";return{execute:function(){}}}));
+//# sourceMappingURL=privacy-legacy-518b8698.js.map
